1220bbe99f67dbd6f0ea60f7666de6ca83a38749
Harleen Monder <harleenmonder@gmail.com>
2025-07-10 16:00:54 +0200

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Harleen Monder on 2025-07-10.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
