9236ee646b93720f630edb410eaf24670d1b51bf
Harleen Monder <harleenmonder@gmail.com>
2025-07-10 16:25:57 +0200

    Added second view

---
+++ Navigation/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  Navigation
+//
+//  Created by Harleen Monder on 2025-07-10.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("This is the Second View!")
+    }
+}
+
+#Preview {
+    SecondView()
+}
